4d5456e6f92949b8666668197e463b2760d88054
Ivo Kořínek <korinivo@fit.cvut.cz>
2022-09-09 17:47:28 +0200

    Initial commit; import Spring template

---
+++ src/main/java/cz/cvut/fit/wikimetric/WikimetricApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.wikimetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimetricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/wikimetric/WikimetricApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.wikimetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimetricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
