68b967e456eaa1dc1812671a42a3769bc698919c
Ivo Kořínek <korinivo@fit.cvut.cz>
2023-02-13 16:16:22 +0100

    Get rid of old tests

--- src/test/java/cz/cvut/fit/wikimetric/model/UserTest.java
+++ src/test/java/cz/cvut/fit/wikimetric/model/UserTest.java
@@ -1,13 +1,8 @@
 package cz.cvut.fit.wikimetric.model;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserTest {
-    @Test
-    void getId() {
-        User user = new User(1L);
-        assert user.getId() == 1;
-    }
+
 }
