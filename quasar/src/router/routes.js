c334b54f6c8073fefdfc2a941680f8165cfcf276
korinivo <119417224+korinivo@users.noreply.github.com>
2023-03-13 14:33:23 +0100

    Oh whoops forgot the routes

--- quasar/src/router/routes.js
+++ quasar/src/router/routes.js
@@ -8,6 +8,7 @@ const routes = [
       { path: '/user',         component: () => import('pages/UsersPage.vue') },
       { path: '/user/new',     component: () => import('pages/UsersNewPage.vue') },
       { path: '/user/tag',     component: () => import('pages/UserTagsPage.vue') },
+      { path: '/event',        component: () => import('pages/EventsPage.vue') },
     ]
   },
 
