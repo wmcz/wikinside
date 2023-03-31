ac1426f9bc5d8302c0f3b6c6cfd4847c75e56c15
korinivo <119417224+korinivo@users.noreply.github.com>
2023-03-31 15:07:55 +0200

    Add project pages II

---
+++ quasar/src/pages/ProjectsPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-gutter-md">
+      <q-list top bordered class="rounded-borders" style="min-width: 600px">
+        <q-item-label header>Projects</q-item-label>
+        <q-input class="q-pa-md" ref="filterRef" v-model="filter" label="Filter">
+          <template v-slot:append>
+            <q-icon v-if="filter !== ''" name="clear" class="cursor-pointer" @click="resetFilter" />
+          </template>
+        </q-input>
+        <q-table :rows="data" :row-key="name" grid style="max-width: 600px" :loading="loading" :filter="filter" :pagination="{ rowsPerPage: 10}">
+          <template v-slot:item="props">
+            <ProjectLink :key="props.row.name" v-bind="props.row" style="width: 600px"/>
+          </template>
+        </q-table>
+      </q-list>
+
+      <q-btn bottom to="/project/new" color="primary">Add</q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import ProjectLink from "components/ProjectLink.vue";
+import {api} from "boot/axios";
+
+export default {
+  name: "ProjectsPage",
+  components: {
+    ProjectLink
+  },
+  data() {
+    return {
+      data: [],
+      loading: true,
+      filter: ''
+    }
+  },
+  mounted() {
+    api.get('projects').then((response) => {
+      this.data = response.data
+      this.loading = false
+    })
+  },
+  methods: {
+    resetFilter: function () {
+      this.filter = ''
+    }
+  }
+}
+</script>
+
