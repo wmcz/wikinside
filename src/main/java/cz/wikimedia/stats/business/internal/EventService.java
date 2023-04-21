d1c82fbae1cb25f4b26f2261dfaf378665bc1a32
korinivo <119417224+korinivo@users.noreply.github.com>
2023-04-21 17:58:11 +0200

    Remove bad revs after updating event data

--- src/main/java/cz/wikimedia/stats/business/internal/EventService.java
+++ src/main/java/cz/wikimedia/stats/business/internal/EventService.java
@@ -3,10 +3,16 @@ package cz.wikimedia.stats.business.internal;
 import cz.wikimedia.stats.dao.EventRepository;
 import cz.wikimedia.stats.model.Event;
 import cz.wikimedia.stats.model.Impact;
+import cz.wikimedia.stats.model.Revision;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService extends InternalService<Event, Long> {
@@ -17,6 +23,18 @@ public class EventService extends InternalService<Event, Long> {
         super(repository);
         this.eventRepository = repository;
         this.impactService = impactService;
+    }
+
+    private boolean isWithinDateRange(Instant timestamp, LocalDate start, LocalDate end) {
+        return timestamp.isAfter(
+                start
+                        .atStartOfDay(ZoneId.systemDefault())
+                        .toInstant()) &&
+                timestamp.isBefore(
+                        end
+                                .plusDays(1)
+                                .atStartOfDay(ZoneId.systemDefault())
+                                .toInstant());
     }
 
     public Collection<Event> findByName(String name) {
@@ -35,5 +53,39 @@ public class EventService extends InternalService<Event, Long> {
                         .reduce(new HashSet<>(),
                                 (acc, revs) -> {acc.addAll(revs); return acc;},
                                 (acc, acc2) -> {acc.addAll(acc2); return acc;}));
+    }
+
+    public void removeNonConformingRevs(Event event) {
+        event
+                .getRevisions()
+                .stream()
+                .filter(r -> !(
+                        isWithinDateRange(r.getTimestamp(), event.getStartDate(), event.getEndDate()) &&
+                        event.getProjects().contains(r.getProject()) &&
+                        event.getParticipants().contains(r.getUser())
+                ))
+                .toList()
+                .forEach(event::removeRevision);
+
+        if (event.getHashtag() != null)
+            // for automatically generated users, remove them if they have no revisions
+            event
+                    .getParticipants()
+                    .retainAll(event
+                            .getRevisions()
+                            .stream()
+                            .map(Revision::getUser)
+                            .collect(Collectors.toSet()));
+    }
+
+    @Override
+    public <S extends Event> Optional<S> update(S event) {
+        Optional<Event> current = findById(event.getId());
+
+        if (current.isEmpty()) return Optional.empty();
+        else {
+            removeNonConformingRevs(event);
+            return super.update(event);
+        }
     }
 }
