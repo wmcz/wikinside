4ad4dc06213dcc8aaee1b6af2677567093a1db28
korinivo <119417224+korinivo@users.noreply.github.com>
2023-04-28 22:41:54 +0200

    More tests

---
+++ src/test/java/cz/wikimedia/stats/business/internal/EventServiceTest.java
@@ -0,0 +1,90 @@
+package cz.wikimedia.stats.business.internal;
+
+import cz.wikimedia.stats.dao.EventRepository;
+import cz.wikimedia.stats.model.Event;
+import cz.wikimedia.stats.model.Project;
+import cz.wikimedia.stats.model.Revision;
+import cz.wikimedia.stats.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@SpringBootTest
+class EventServiceTest {
+
+    @MockBean
+    EventRepository repository;
+
+    EventService service;
+
+    @Test
+    void update() {
+        service = new EventService(repository);
+        Mockito.when(repository.save(ArgumentMatchers.any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        Project project1 = new Project(0L, "name1", "path1");
+        Project project2 = new Project(1L, "name2", "path2");
+
+
+        User user1 = new User(1001L, "one");
+        User user2 = new User(1002L, "two");
+        User user3 = new User(1003L, "three");
+
+        Revision rev0 = new Revision(0L, 0L, 0L, 0L, 0L, user1, new HashSet<>(), project1, Instant.parse("1999-06-01T00:00:00.00Z"), "summary");
+        Revision rev1 = new Revision(0L, 1L, 0L, 0L, 0L, user2, new HashSet<>(), project2, Instant.parse("1999-06-01T00:00:00.00Z"), "summary");
+        Revision rev2 = new Revision(0L, 2L, 0L, 0L, 0L, user3, new HashSet<>(), project1, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev3 = new Revision(0L, 3L, 0L, 0L, 0L, user1, new HashSet<>(), project2, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev4 = new Revision(0L, 4L, 0L, 0L, 0L, user2, new HashSet<>(), project1, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev5 = new Revision(0L, 5L, 0L, 0L, 0L, user3, new HashSet<>(), project2, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev6 = new Revision(0L, 6L, 0L, 0L, 0L, user1, new HashSet<>(), project1, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev7 = new Revision(0L, 7L, 0L, 0L, 0L, user2, new HashSet<>(), project2, Instant.parse("2000-06-01T00:00:00.00Z"), "summary");
+        Revision rev8 = new Revision(0L, 8L, 0L, 0L, 0L, user3, new HashSet<>(), project1, Instant.parse("2001-06-01T00:00:00.00Z"), "summary");
+        Revision rev9 = new Revision(0L, 9L, 0L, 0L, 0L, user1, new HashSet<>(), project2, Instant.parse("2001-06-01T00:00:00.00Z"), "summary");
+
+        Set<Revision> revs = Set.of(rev0, rev1, rev2, rev3, rev4, rev5, rev6, rev7, rev8, rev9);
+
+        Event event1 = new Event(1L, null, "one", null, LocalDate.of(2000, 1, 1), LocalDate.of(2000, 12, 31), new HashSet<>(Set.of(user1, user2)), new HashSet<>(Set.of(project1)), new HashSet<>(revs));
+        Event event2 = new Event(2L, null, "one", null, LocalDate.of(2000, 1, 1), LocalDate.of(2000, 12, 31), new HashSet<>(Set.of(user2, user3)), new HashSet<>(Set.of(project2)), new HashSet<>(revs));
+        Event event3 = new Event(3L, null, "one", null, LocalDate.of(1999, 1, 1), LocalDate.of(2001, 12, 31), new HashSet<>(Set.of(user1, user2, user3)), new HashSet<>(Set.of(project1, project2)), new HashSet<>(revs));
+        Event event4 = new Event(4L, null, "one", "##", LocalDate.of(1999, 1, 1), LocalDate.of(1999, 12, 31), new HashSet<>(Set.of(user1, user2, user3)), new HashSet<>(Set.of(project1)), new HashSet<>(revs));
+
+        Mockito.when(repository.existsById(ArgumentMatchers.any())).thenReturn(true);
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(event1));
+        Mockito.when(repository.findById(2L)).thenReturn(Optional.of(event2));
+        Mockito.when(repository.findById(3L)).thenReturn(Optional.of(event3));
+        Mockito.when(repository.findById(4L)).thenReturn(Optional.of(event4));
+
+
+        event1 = service.update(event1).get();
+        event2 = service.update(event2).get();
+        event3 = service.update(event3).get();
+        event4 = service.update(event4).get();
+
+        //event1 contains only rev4 and rev6, etc. etc.
+        Assertions.assertTrue(event1.getRevisions().containsAll(List.of(rev4, rev6)));
+        Assertions.assertFalse(event1.getRevisions().retainAll(List.of(rev4, rev6)));
+
+        Assertions.assertTrue(event2.getRevisions().containsAll(List.of(rev5, rev7)));
+        Assertions.assertFalse(event2.getRevisions().retainAll(List.of(rev5, rev7)));
+
+        Assertions.assertTrue(event3.getRevisions().containsAll(revs));
+
+        Assertions.assertTrue(event4.getRevisions().contains(rev0));
+        Assertions.assertFalse(event4.getRevisions().retainAll(List.of(rev0)));
+
+        Assertions.assertTrue(event4.getParticipants().contains(user1));
+        Assertions.assertFalse(event4.getParticipants().retainAll(List.of(user1)));
+
+    }
+}
---
+++ src/test/java/cz/wikimedia/stats/business/internal/UserServiceTest.java
@@ -0,0 +1,170 @@
+package cz.wikimedia.stats.business.internal;
+
+import cz.wikimedia.stats.api.client.dto.GlobalUserInfo;
+import cz.wikimedia.stats.business.external.WmUserService;
+import cz.wikimedia.stats.dao.EventRepository;
+import cz.wikimedia.stats.dao.RevisionRepository;
+import cz.wikimedia.stats.dao.UserRepository;
+import cz.wikimedia.stats.model.Event;
+import cz.wikimedia.stats.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
+import org.mockito.AdditionalMatchers;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @MockBean
+    UserRepository repository;
+
+    @MockBean
+    EventRepository eventRepository;
+
+    @MockBean
+    WmUserService wmUserService;
+
+    @MockBean
+    RevisionRepository revisionRepository;
+
+    UserService service;
+
+    @BeforeEach
+    void setUp() {
+        Event event1 = new Event(1001L, null, "one", null, null, null, new HashSet<>(), null, null);
+        Event event2 = new Event(1002L, null, "two", null, null, null, new HashSet<>(), null, null);
+
+        User user1 = new User(1L, 1L, "one",   Instant.EPOCH, new HashSet<>(), new HashSet<>());
+        User user2 = new User(2L, 2L, "two",   Instant.EPOCH, new HashSet<>(), new HashSet<>());
+        User user3 = new User(3L, 3L, "three", Instant.EPOCH, new HashSet<>(), new HashSet<>(List.of(event1, event2)));
+        User user4 = new User(4L, 4L, "four",  Instant.EPOCH, new HashSet<>(), new HashSet<>());
+        User user5 = new User(5L, 5L, "five",  Instant.EPOCH, new HashSet<>(), new HashSet<>());
+
+        Mockito.when(wmUserService.getGlobalUserInfo("one")).thenReturn(new GlobalUserInfo("home", 1L, Instant.EPOCH, "one"));
+        Mockito.when(wmUserService.getGlobalUserInfo("two")).thenReturn(new GlobalUserInfo("home", 2L, Instant.EPOCH, "two"));
+        Mockito.when(wmUserService.getGlobalUserInfo("three")).thenReturn(new GlobalUserInfo("home", 3L, Instant.EPOCH, "three"));
+        Mockito.when(wmUserService.getGlobalUserInfo("four")).thenReturn(new GlobalUserInfo("home", 4L, Instant.EPOCH, "four"));
+        Mockito.when(wmUserService.getGlobalUserInfo("five")).thenReturn(new GlobalUserInfo("home", 5L, Instant.EPOCH, "five"));
+        Mockito.when(wmUserService.getGlobalUserInfo("six")).thenReturn(null);
+
+
+        Mockito.when(wmUserService.updateNames(Mockito.any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+        Mockito.when(wmUserService.getLocalId("one")).thenReturn(1L);
+        Mockito.when(wmUserService.getLocalId("two")).thenReturn(2L);
+        Mockito.when(wmUserService.getLocalId("three")).thenReturn(3L);
+        Mockito.when(wmUserService.getLocalId("four")).thenReturn(4L);
+        Mockito.when(wmUserService.getLocalId("five")).thenReturn(5L);
+
+
+        Mockito.when(eventRepository.existsById(ArgumentMatchers.any())).thenReturn(true);
+        Mockito.when(eventRepository.findById(1001L)).thenReturn(Optional.of(event1));
+        Mockito.when(eventRepository.findById(1002L)).thenReturn(Optional.of(event2));
+
+        Mockito.when(eventRepository.save(ArgumentMatchers.any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        Mockito.doNothing().when(revisionRepository).deleteById(ArgumentMatchers.any());
+        Mockito.doNothing().when(repository).deleteById(ArgumentMatchers.any());
+
+
+        Mockito.when(repository.save(ArgumentMatchers.any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+        Mockito.when(repository.findByUsername("one")).thenReturn(Optional.of(user1));
+        Mockito.when(repository.findByUsername("two")).thenReturn(Optional.of(user2));
+        Mockito.when(repository.findByUsername("three")).thenReturn(Optional.of(user3));
+        Mockito.when(repository.findByUsername("four")).thenReturn(Optional.of(user4));
+        Mockito.when(repository.findByUsername("five")).thenReturn(Optional.empty());
+        Mockito.when(repository.findByUsername("six")).thenReturn(Optional.empty());
+
+        Mockito.when(repository.existsById(AdditionalMatchers.lt(5L))).thenReturn(true);
+        Mockito.when(repository.existsById(5L)).thenReturn(false).thenReturn(true);
+        Mockito.when(repository.existsById(AdditionalMatchers.gt(5L))).thenReturn(false);
+
+
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(user1));
+        Mockito.when(repository.findById(2L)).thenReturn(Optional.of(user2));
+        Mockito.when(repository.findById(3L)).thenReturn(Optional.of(user3));
+        Mockito.when(repository.findById(4L)).thenReturn(Optional.of(user4));
+        Mockito.when(repository.findById(5L)).thenReturn(Optional.of(user5));
+
+
+
+        service = new UserService(repository, eventRepository, wmUserService, revisionRepository);
+    }
+    @Test
+    void createFromGlobalUser() {
+        Mockito.when(repository.existsById(1L)).thenReturn(false).thenReturn(true);
+        Mockito.when(repository.existsById(2L)).thenReturn(true);
+
+        Assertions.assertTrue(service.createFromGlobalUser(new User(null, "one")).isPresent());
+        Assertions.assertTrue(service.createFromGlobalUser(new User(null, "two")).isEmpty());
+
+        Mockito.verify(wmUserService, Mockito.atLeastOnce()).getGlobalUserInfo("one");
+        Mockito.verify(wmUserService, Mockito.atLeastOnce()).getGlobalUserInfo("two");
+        Mockito.verify(repository, Mockito.atLeastOnce()).save(new User(1L, "one"));
+        Mockito.verify(repository, Mockito.never()).save(new User(2L, "two"));
+    }
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+        Mockito.verifyNoMoreInteractions(eventRepository);
+
+        service.deleteById(3L);
+        Mockito.verify(eventRepository, Mockito.atLeast(2)).save(ArgumentMatchers.any());
+
+    }
+
+    @Test
+    void create() {
+        Mockito.when(repository.existsById(6L)).thenReturn(false).thenReturn(true);
+        Mockito.when(repository.findById(6L)).thenReturn(Optional.of(new User(6L, "six")));
+
+        Assertions.assertTrue(service.create(new User(1L, "one")).isEmpty());
+
+        Mockito.verify(repository, Mockito.never()).save(new User(1L, "one"));
+
+        Assertions.assertTrue(service.create(new User(5L, "five")).isPresent());
+        Mockito.verify(repository, Mockito.atLeastOnce()).save(new User(5L, "five"));
+
+        Assertions.assertTrue(service.create(new User(6L, 6L, "six",  Instant.EPOCH, new HashSet<>(), new HashSet<>(List.of(new Event(1001L, null, null, null, null, null, new HashSet<>(), null, null))))).isPresent());
+        Mockito.verify(repository, Mockito.atLeastOnce()).save(new User(6L, "six"));
+        Mockito.verify(eventRepository, Mockito.atLeastOnce()).save(new Event(1001L, null, null, null, null, null, null, null, null));
+    }
+
+    @Test
+    void update() {
+        Assertions.assertTrue(service.update(new User(5L, "five")).isEmpty());
+        Mockito.verify(repository, Mockito.never()).save(new User(5L, "five"));
+
+        Assertions.assertTrue(service.update(new User(1L, "one")).isPresent());
+        Mockito.verify(repository, Mockito.atLeastOnce()).save(new User(1L, "one"));
+
+        Assertions.assertTrue(service.update(new User(3L, 3L, "six",  Instant.EPOCH, new HashSet<>(), new HashSet<>())).isPresent());
+        Mockito.verify(repository, Mockito.atLeastOnce()).save(new User(3L, "three"));
+        Mockito.verify(eventRepository, Mockito.atLeastOnce()).save(new Event(1001L, null, null, null, null, null, null, null, null));
+        Mockito.verify(eventRepository, Mockito.atLeastOnce()).save(new Event(1002L, null, null, null, null, null, null, null, null));
+    }
+
+    @Test
+    void processUser() {
+        Assertions.assertNull(service.processUser("127.0.0.1"));
+        Assertions.assertNull(service.processUser("0000:0000:0000:0000:0000:0000:0000:0001"));
+
+        Assertions.assertEquals(new User(1L, "one"), service.processUser("one"));
+
+        Mockito.verifyNoInteractions(wmUserService);
+
+        Assertions.assertEquals(new User(5L, "five"), service.processUser("five"));
+
+        Mockito.verify(wmUserService).getGlobalUserInfo("five");
+    }
+}
