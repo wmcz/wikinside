49dee368d497bee6be5c2871d59d40adfca8d057
korinivo <korinivo@fit.cvut.cz>
2024-07-30 17:19:53 +0200

    Make the config a bean

--- src/main/java/cz/wikimedia/stats/HttpMessageConverterConfig.java
+++ src/main/java/cz/wikimedia/stats/HttpMessageConverterConfig.java
@@ -1,19 +1,20 @@
 package cz.wikimedia.stats;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Configuration
-public class HttpMessageConverterConfigurer implements WebMvcConfigurer {
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+@Configuration(proxyBeanMethods = false)
+public class HttpMessageConverterConfig {
+    @Bean
+    public HttpMessageConverter<Object> converter() {
         MappingJackson2HttpMessageConverter htmlTextConverter = new MappingJackson2HttpMessageConverter();
         htmlTextConverter.setSupportedMediaTypes(List.of(MediaType.TEXT_HTML));
-        converters.add(htmlTextConverter);
+        htmlTextConverter.getSupportedMediaTypes().forEach(System.out::println);
+        return htmlTextConverter;
     }
 }
